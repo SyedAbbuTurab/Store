db87ba7dac3af0686d7d82fdf3619e5adfdba9b8
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-18 21:23:55 -0400

    Initial commit project initialised.

---
+++ src/main/java/com/codewithturab/Store/model/Book.java
@@ -0,0 +1,9 @@
+package com.codewithturab.Store.model;
+
+import  jakarta.persistence.Entity;
+import  jakarta.persistence.GeneratedValue;
+import  jakarta.persistence.Id;
+import  jakarta.persistence.GenerationType;
+
+public class Book {
+}
