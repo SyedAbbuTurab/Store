ec883d9cdc4eca7c9c3509ff4c3ea43038d5b719
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-18 22:38:45 -0400

    Added Book schema

--- src/main/java/com/codewithturab/Store/model/Book.java
+++ src/main/java/com/codewithturab/Store/model/Book.java
@@ -1,15 +1,45 @@
 package com.codewithturab.Store.model;
 
-import  jakarta.persistence.Entity;
-import  jakarta.persistence.GeneratedValue;
-import  jakarta.persistence.Id;
-import  jakarta.persistence.GenerationType;
+import  org.springframework.data.annotation.Id;
+import  org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+
+
+@Document(collection = "books")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  long id;
+    private String id;
     private String title;
     private String author;
+
+    public Book() {}
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
--- src/main/java/com/codewithturab/Store/repository/BookRepository.java
+++ src/main/java/com/codewithturab/Store/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.codewithturab.Store.repository;
 
 import com.codewithturab.Store.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
 
