8c3726d18ab07b77bc00730c596ad4a0649c2e59
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-22 22:46:07 -0400

    Added route to connect with server

--- src/main/java/com/codewithturab/Store/controller/BookController.java
+++ src/main/java/com/codewithturab/Store/controller/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService service;
