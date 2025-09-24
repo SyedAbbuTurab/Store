377ba112fc00e142627e85815a5d6575657e202a
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-23 23:14:08 -0400

    Added exception handler initialisation

---
+++ src/main/java/com/codewithturab/Store/exception/GLobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.codewithturab.Store.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Centralized error handler for all controllers
+@ControllerAdvice
+public class GLobalExceptionHandler {
+
+}
