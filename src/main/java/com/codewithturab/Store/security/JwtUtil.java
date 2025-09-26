585cd53533d355cf192dbfcf209d0854dc3f227c
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-25 23:39:26 -0400

    added jwt sign and token validation

--- src/main/java/com/codewithturab/Store/security/JwtUtil.java
+++ src/main/java/com/codewithturab/Store/security/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.codewithturab.Store.security;
 
+import com.codewithturab.Store.model.Book;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
@@ -10,5 +11,28 @@ public class JwtUtil {
     private static  final String SECRET_KEY = "Crazpeeps@1";
     private static  final long EXPIRATION = 1000 * 60 * 60;
 
+    public static  String generateToke(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public static Boolean isTokenValid(String token) {
+        return !getClaims(token).getExpiration().before(new Date());
+    }
+
+    private static Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJwt(token)
+                .getBody();
+    }
 
 }
