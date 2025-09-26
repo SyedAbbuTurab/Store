388b96f3497bd7901945fe38a1104181b594e689
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-25 23:16:30 -0400

    created interface for user model

---
+++ src/main/java/com/codewithturab/Store/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codewithturab.Store.repository;
+
+import com.codewithturab.Store.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends MongoRepository<User, String>{
+    Optional<User> findByUsername(String username);
+}
