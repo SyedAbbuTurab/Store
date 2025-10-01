52d1dea6024a0eeaac820f0128be88a66e6d7fb5
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-30 23:09:33 -0400

    base route to test admin role service

--- src/main/java/com/codewithturab/Store/security/JwtAuthHelper.java
+++ src/main/java/com/codewithturab/Store/security/JwtAuthHelper.java
@@ -1,4 +1,10 @@
 package com.codewithturab.Store.security;
 
 public class JwtAuthHelper {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthHelper(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 }
