1fd775b15036a9a44211ea1726175a4ce8c8168e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-05 14:42:21 -0400

    Created base setup

--- order-service/src/main/java/com/codewithturab/order/model/Order.java
+++ order-service/src/main/java/com/codewithturab/order/model/Order.java
@@ -1,5 +1,14 @@
 package com.codewithturab.order.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+@Document("orders")
 public class Order {
+
+    @Id
+    private String id;
+    private String productName;
+    private int quantity;
 
 }
