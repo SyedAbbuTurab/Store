4798224e05255348468c5cd75082ca2c23d893a5
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-04 21:25:52 -0400

    Created user DTO along with setters and getters

--- common-lib/src/main/java/com/codewithturab/common/dto/UserDTO.java
+++ common-lib/src/main/java/com/codewithturab/common/dto/UserDTO.java
@@ -1,4 +1,40 @@
 package com.codewithturab.common.dto;
 
 public class UserDTO {
+
+    private String id;
+    private String username;
+    private String role;
+
+    public UserDTO() {}
+
+    public UserDTO(String id, String username, String role) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
