1f2a453f8d55200b0f0a62dc5cd7b776228c96c2
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-06 19:33:34 -0400

    auth client services

--- order-service/src/main/java/com/codewithturab/order/client/AuthClient.java
+++ order-service/src/main/java/com/codewithturab/order/client/AuthClient.java
@@ -1,4 +1,25 @@
 package com.codewithturab.order.client;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
 public class AuthClient {
-}
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final String authServiceUrl = "http://localhost:8080/api/auth/validate";
+
+    public Boolean validateToken(String token) {
+        try {
+            ResponseEntity<Boolean> response = restTemplate
+                    .postForEntity(authServiceUrl, token, Boolean.class);
+            return response.getBody() != null && response.getBody();
+        } catch (Exception e) {
+            System.err.println("Failed to validate token: " + e.getMessage());
+            return false;
+        }
+        }
+    }
+
