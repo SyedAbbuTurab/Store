b4f1b5a320edaa8a7d3d3d483d2899ff268c57d6
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-08 22:11:20 -0400

    model for inventory created

--- inventory-service/src/main/java/com/codewithturab/inventory/model/inventory.java
+++ inventory-service/src/main/java/com/codewithturab/inventory/model/inventory.java
@@ -3,6 +3,23 @@ package com.codewithturab.inventory.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Document(collection = "inventory")
 public class inventory {
+
+    @Id
+    private String id;
+    private String productName;
+    private int quantity;
+    private double price;
+
+    public inventory() {
+
+    }
+
+    public inventory(String productName, int quantity, double price ) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
