e7e5aba038b9b80da263deda827be157b9d81ebc
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-08 22:05:23 -0400

    import dependencies

---
+++ inventory-service/src/main/java/com/codewithturab/inventory/model/inventory.java
@@ -0,0 +1,8 @@
+package com.codewithturab.inventory.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+public class inventory {
+}
