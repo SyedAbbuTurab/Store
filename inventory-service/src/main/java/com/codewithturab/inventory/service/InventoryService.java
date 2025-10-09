7117aa5cebd5e1260d9f9a1a0f4a155868bb9d06
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-08 22:25:48 -0400

    inventory serivce initialisation

---
+++ inventory-service/src/main/java/com/codewithturab/inventory/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.codewithturab.inventory.service;
+
+import com.codewithturab.inventory.model.inventory;
+import com.codewithturab.inventory.repository.inventoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+public class InventoryService {
+
+    private final inventoryRepository repo;
+
+    public InventoryService(inventoryRepository repo) {
+        this.repo = repo;
+    }
+
+
+}
