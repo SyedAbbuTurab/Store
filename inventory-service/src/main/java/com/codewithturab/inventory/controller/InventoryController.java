d6b3ab2ea883da7b044f3e7e76ba2bd893fab775
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-11 22:55:58 -0400

    Controller for invemtory

--- inventory-service/src/main/java/com/codewithturab/inventory/controller/InventoryController.java
+++ inventory-service/src/main/java/com/codewithturab/inventory/controller/InventoryController.java
@@ -1,4 +1,39 @@
 package com.codewithturab.inventory.controller;
 
+import com.codewithturab.inventory.model.inventory;
+import com.codewithturab.inventory.service.InventoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
 public class InventoryController {
+
+    private final InventoryService service;
+
+    public InventoryController(InventoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<inventory> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public inventory add(@RequestBody inventory item) {
+        return service.add(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
+    @GetMapping("/{name}")
+    public inventory getByProductName(@PathVariable String name) {
+        return service.findByProduct(name)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
 }
