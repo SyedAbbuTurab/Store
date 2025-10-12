4e3327b7587e139875cf61d7b397448de7fbbdfb
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-12 14:27:17 -0400

    Repos modfication

--- order-service/src/main/java/com/codewithturab/order/repository/OrderRepository.java
+++ order-service/src/main/java/com/codewithturab/order/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.codewithturab.order.repository;
 
 import com.codewithturab.order.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface OrderRepository extends MongoRepository<Order, String> {
+    List<Order> findByUserId(String userId);
 }
